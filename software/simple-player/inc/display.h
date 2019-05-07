e77dc587a3ac5024bad647936c307d6746cabea4
Rahul Butani <rr.butani@gmail.com>
2019-05-07 01:19:12 -0500

    simple-player: some boilerplate

---
+++ software/simple-player/inc/display.h
@@ -0,0 +1,12 @@
+/*
+ * Routines that draw and update the user interface on the display.
+ *
+ * This module makes use of esp32-tft to communicate with the display; though
+ * esp32-tft supports many displays, this module assumes an ST7735R connected
+ * according to the pins defined (and configurable) in the pin_map component.
+ */
+
+#ifndef _SP_DISPLAY_
+#define _SP_DISPLAY_
+
+#endif /* _SP_BUTTONS_ */
